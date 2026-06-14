44ccea5b0ee460356e90fbd81d68b54c579c3626
hakimcolor <hakimcolor777@gmail.com>
2026-06-15 00:53:31 +0600

    add database postgresql

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,26 @@
-import express, { type Application, type Request, type Response } from "express";
-import Router from "./router/server.route.js";
-import dotenv from "dotenv";
+import express, {
+  type Application,
+  type Request,
+  type Response,
+} from 'express';
+import Router from './router/server.route.js';
+import dotenv from 'dotenv';
 dotenv.config();
 
-const app : Application = express();
-const port : number = Number(process.env.PORT);
+const app: Application = express();
+const port: number = Number(process.env.PORT) || 5000;
 
 app.use(express.json());
 
 app.use('/api', Router);
 
-app.get('/', (req : Request, res : Response) => {
-    res.status(200).json({
-        success: true,
-        message : "DevPulse is Running successfully",
-    })
+app.get('/', (req: Request, res: Response) => {
+  res.status(200).json({
+    success: true,
+    message: 'DevPulse is Running successfully',
+  });
 });
 
 app.listen(port, () => {
-    console.log(`Server is Running on ${port}`);
+  console.log(`Server is Running on ${port}`);
 });
